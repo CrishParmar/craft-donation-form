d6b95c005be67d22d27ac90417c1cd504ab26764
David <68780925+CrishParmar@users.noreply.github.com>
2023-02-03 23:29:53 +0530

    Update plugin

---
+++ src/donation-form.php
@@ -0,0 +1,14 @@
+<?php
+namespace donationform;
+
+use Craft;
+
+class Donation extends \craft\base\Donation
+{
+    public function init()
+    {
+        parent::init();
+
+        // Custom initialization code goes here...
+    }
+}
